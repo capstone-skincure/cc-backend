fa5c88f751ab2dc579e95774a726836cb032827b
unknown <nisamafrokhatunaliyah@gmail.com>
2024-12-05 18:15:15 +0700

    Update env & server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const handleError = require('./src/utils/errorHandler');
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 
 app.use(express.json());
 
